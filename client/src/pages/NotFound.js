9424919ee8ca5b903fbd11bb31ae4b50a1c85dbc
Sicor <atrem641@gmail.com>
2023-12-20 21:39:39 +0300

    Update NotFound.js

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+
+const NotFound = () => {
+    return(
+        <>
+        <Header />
+            
+        <Footer />
+        </>
+    )
+
+}
+
+export default NotFound;
