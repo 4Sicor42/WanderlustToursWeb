6d9cb993711a0d39018fb5a1fc1866dd5b788ef0
Sicor <atrem641@gmail.com>
2024-01-06 02:27:56 +0300

    feat: added new review item

---
+++ client/src/components/Common/ReviewItem.js
@@ -0,0 +1,38 @@
+import StarIcon from '@mui/icons-material/Star';
+
+
+
+
+function ReviewItem({review}) {
+
+    const { heading, description, rating, name, company, image } = review;
+  
+    return (
+
+                        <div className='box'>
+                            <div className='header'>
+                             <span className='heading'>“{heading}”</span>
+                              <p className='desc'>{description}
+                              </p>
+                            </div>
+                            <div className='rating_stars'>
+                              {[...Array(rating)].map(() => 
+                                <StarIcon />
+                              )}
+                            </div>
+                            <div className='company_name'>
+                               <h4>{name}</h4>
+                               <p>{company} </p>
+                               
+                            </div>
+                            <div className='bottom_img'>
+                            <img src={image} alt={heading}/>
+                            </div>
+                        </div>
+
+    )
+  
+  }
+
+
+  export default ReviewItem;
