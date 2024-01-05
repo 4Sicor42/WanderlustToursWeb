58ac557aef71d31d984e9002329aa83170d6b99f
Sicor <atrem641@gmail.com>
2024-01-06 02:31:48 +0300

    fix: moved styles to compoments

--- client/src/styles/styles.js
+++
@@ -1,6 +0,0 @@
-import "./index.css";
-import "./components/HeaderStyles.css";
-import "./components/FooterStyles.css";
-import "./components/FormStyles.css";
-import "./components/SwiperStyles.css";
-import "./components/HomeStyles.css";
