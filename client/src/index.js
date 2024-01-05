349f65eca39d05d643bdda49b30f4199f2703686
Sicor <atrem641@gmail.com>
2024-01-06 02:30:38 +0300

    feat: added new imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import "./styles/styles";
-import theme from "./Theme";
-import Login from "./pages/login";
-import SignUp from "./pages/signup";
-import NotFound from "./pages/NotFound"
-import Excursion from "./pages/excursion"
-import Search from "./pages/searchExc"
-import Home from "./pages/homepage"
-import Work from "./pages/work"
-import Story from "./pages/story"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import UserStore from "./store/UserStore";
+import ExcursionStore from './store/ExcurionStore';
+import ReviewsStore from './store/ReviewsStore';
 
-import UserStore from './store';
-import { Provider } from 'mobx-react';
+export const Context = React.createContext(null);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider userStore={UserStore}>
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/excursion"element={<Excursion/>}/>
-          <Route path="/search"element={<Search/>}/>
-          <Route path="/story"element={<Story/>}/>
-          <Route path="/work"element={<Work/>}/>
-          <Route path="*"element={<NotFound/>}/>
-        </Routes>
-      </ThemeProvider>
+    <Context.Provider value={{
+      user: new UserStore(),
+      store:new ExcursionStore(),
+      reviews: new ReviewsStore()
+      }}>
+      <App />
+      </Context.Provider>
     </BrowserRouter>
   </React.StrictMode>
-  </Provider>
 );
 
