2b99fd3edcf1381fefbb8d9a9556b61003127d09
Sicor <atrem641@gmail.com>
2024-01-06 02:32:18 +0300

    fix: moved to components floder

--- client/src/pages/signup.js
+++
@@ -1,8 +0,0 @@
-import SignupFlow from "../components/Flow/SignupFlow";
-import SignupForm from "../components/Forms/SignupForm";
-
-const SignUp = () => {
-  return <SignupFlow form={<SignupForm />} />;
-};
-
-export default SignUp;
