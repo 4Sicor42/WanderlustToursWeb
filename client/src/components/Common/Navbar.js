349c464a6daacc77953057311236ac9effd8bb51
Sicor <atrem641@gmail.com>
2024-01-06 13:58:27 +0300

    fix: fixed pathes

--- client/src/components/Common/Navbar.js
+++ client/src/components/Common/Navbar.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect, useContext} from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import Logo from '../Assets/Images/Logo.png';
-import NavLogo from '../Assets/Images/Navlogo.png';
 import MenuIcon from '@mui/icons-material/Menu';
 import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
 import { Context } from '../../index.js';
@@ -14,13 +13,13 @@ const Navbar = observer(() => {
   const [navbartogle, setToggler] = useState(false);
 
   const [scroll, setScroll] = useState(false);
- useEffect(() => {
-   window.addEventListener("scroll", () => {
-     setScroll(window.scrollY > 70);
-   });
- }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 70);
+    });
+  }, []);
   let location=useLocation();
-  const excursionClass = location.pathname.includes("/flight") ? "flight_active_link" : "";
+  const excursionClass = location.pathname.includes("/excursion") ? "flight_active_link" : "";
  
 
 const handleToggler=()=>{
@@ -38,7 +37,7 @@ const handleToggler=()=>{
       <div className={`navbar_inner ${navbartogle ? "toggle_navbar" : ""} `}>
         <button className='navbar_close_btn' onClick={()=>setToggler(false)}><MenuIcon /></button>
         <div className='navbar_links'>
-           <Link to="flight/list" className={excursionClass}> <AirportShuttleIcon className='aeroplane' /> Find Flight</Link>
+           <Link to="excursion/list" className={excursionClass}> <AirportShuttleIcon className='aeroplane' /> Find excursion</Link>
            
 
          </div>
@@ -48,7 +47,7 @@ const handleToggler=()=>{
            
          </div>
          <div className='landing_logo'>
-           <Link  to="/"><img   src={scroll ? Logo : NavLogo} alt="navbar_logo"></img></Link>
+           <Link  to="/"><img   src={Logo} alt="navbar_logo"></img></Link>
          </div>
          {user.isAuth?
           <div className='navbar_auth_buttons'>
