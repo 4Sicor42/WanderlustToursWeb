814a7e6aa2a895cb7a2dcdd3388edb4f06b4442f
Sicor <atrem641@gmail.com>
2024-02-01 00:12:22 +0300

    feat: added validation

--- client/src/Components/Common/HistoryComponent.js
+++ client/src/Components/Common/HistoryComponent.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import Table from '@mui/joy/Table';
 
-
-
-const HistoryComponent = () => {
-
+const HistoryComponent = ({ history }) => {
   return (
-    <div >
-      
-    </div>
-  )
-}
+    <div className="history-table">
+      <h2>Buying history</h2>
+          <Table
+          borderAxis="none"
+          color="neutral"
+          size="md"
+          stickyFooter
+          stickyHeader
+          variant="soft"
+          className = "table"
+          >
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Meet at</th>
+            <th>Price</th>
+            <th>Country</th>
+            <th>Adress</th>
+            <th>Manager</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.meet}</td>
+              <td>{item.excursion.price}</td>
+              <td>{item.excursion.country}</td>
+              <td>{item.excursion.adress}</td>
+              <td>{item.excursion.manager}</td>
+            </tr>
+          ))}
+        </tbody>
 
-export default HistoryComponent
+      </Table>
+    </div>
+  );
+};
+
+export default HistoryComponent;
--- client/src/components/Pages/Excursion/ExcursionBooking.js
+++ client/src/components/Pages/Excursion/ExcursionBooking.js
@@ -1,27 +1,45 @@
 import React, {useContext} from 'react';
 import { Container,Row } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Leftarrow from '../../Assets/Images/icons/left.png';
 import { Context } from '../../../index.js';
 import UnauthorizedComponent from '../../Common/UnauthorizedComponent';
 import NotFound from '../NotFound';
+import { bookExcursion } from '../../../http/ExcursionAPI';
 // icons 
 import ApartmentIcon from '@mui/icons-material/Apartment';
 
 const ExcursionBooking = () => {
     const { user } = useContext(Context);
+    const navigate = useNavigate();
     const location = useLocation();
     const excursion = location.state?.excursion;
 
-    const handleClick = () => {
-        navigate(`/excursion/${excursion.id}/overview`, {
-          replace: true,
-          state: { excursion } 
-        });
-      }
+    const handleClick = async () => {
+        try {
+            var code =excursion.id^excursion.averageRating*user.id^excursion.averageRating*excursion.averageRating*excursion.count^excursion.averageRating/24;
+            const date = new Date(excursion.date);
+            const hours = date.getUTCHours();
+            const minutes = date.getUTCMinutes();
+            const formattedTime = `${hours}:${minutes}`;
+            const data = await bookExcursion(user.id, excursion.id,formattedTime,code,excursion.name);
+            
+            if (data.history.excursionId == null){
+                alert("Error: excursionId is "+data.history.excursionId  )
+            }
+            else{
+                navigate(`/excursion/${excursion.id}/overview`, {
+                    replace: true,
+                    state: { excursion },
+                });  
+            }
+            
 
-    let navigate=useNavigate();   
+        } catch (error) {
+            alert("Error:"+error)
+        }
+      };
+
 
     if (!user.isAuth) {
         return <UnauthorizedComponent/>;
@@ -47,14 +65,14 @@ const ExcursionBooking = () => {
                                     <img src={process.env.REACT_APP_API_URL + excursion.img} alt={excursion.name}></img>
                                     <div className='name'>
                                         <h5>{excursion.country}</h5>
-                                        <p>{excursion.adress}</p>
+                                        <p>{excursion.address}</p>
                                     </div>
                                 </div>
                                 
 
                             </div>
                             <div className='in_out_time_container'>
-                                <div className='time_Div'><span>{excursion.date}</span> <p>Check In</p> </div>
+                                <div className='time_Div'><span>{excursion.date.split('T')[0]}</span> <p>Check In</p> </div>
                                 <div className='plane_icons'>
                                     <img src={Leftarrow} alt='left'></img>
                                         <ApartmentIcon className='plane' />
@@ -72,7 +90,7 @@ const ExcursionBooking = () => {
                                 <p>{excursion.manager}</p>
                                 <h4>{excursion.name}</h4>
                                 <div className='review'>
-                                    <span className='review_number'>{excursion.rating}</span>
+                                    <span className='review_number'>{excursion.averageRating}</span>
                                 </div>
                             </div>
                         </div>
@@ -89,7 +107,7 @@ const ExcursionBooking = () => {
                         </div>
                     </div>
                 </div>
-                <button onClick={handleClick} className="overview_btn">Overview your booking</button>
+                <button onClick={handleClick} className="overview_btn">Book trip</button>
             </Row>
         </Container>
     </div>
