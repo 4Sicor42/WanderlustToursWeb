2bcab3fe3d54bfa1b57fac190f1131c477bce064
Sicor <atrem641@gmail.com>
2024-01-31 17:14:22 +0300

    fix: fixed cards wrapper

--- client/src/components/Pages/Account/Account.js
+++ client/src/components/Pages/Account/Account.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import Profile from './Profile.js'
-
+import React, { useContext, useState } from 'react';
+import Profile from './Profile.js';
+import { Context } from '../../../index';
+import UnauthorizedComponent from '../../Common/UnauthorizedComponent';
+import AccountHistory from './AccountHistory.js';
 
 const Account = () => {
-  
-  return (
-    <div className='account_wrapper'>
-      <Profile/>
-      
-    </div>
-  )
-}
+  const { user } = useContext(Context);
+  const [activeTab, setActiveTab] = useState('profile');
 
-export default Account
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  if (!user.isAuth) {
+    return <UnauthorizedComponent />;
+  }
+
+  return (
+    <div className="account_wrapper">
+      <div className="account-tab-wrapper">
+        <button
+          className={`account-tab ${activeTab === 'profile' ? 'active' : ''}`}
+          onClick={() => handleTabClick('profile')}
+        >
+          Profile
+        </button>
+
+        <button
+          className={`account-tab ${activeTab === 'history' ? 'active' : ''}`}
+          onClick={() => handleTabClick('history')}
+        >
+          History
+        </button>
+      </div>
+
+      {activeTab === 'profile' && <Profile />}
+      {activeTab === 'history' && <AccountHistory />}
+    </div>
+  );
+};
+
+export default Account;
